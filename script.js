8e80429ec32c15a5f583de6b2fd39c096a928a73
Gayathri-KS101 <gayathrikssrr@gmail.com>
2024-11-04 21:37:28 +0530

    initcommit

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-function adjustHeight() {
-    // Get the viewport height and set it to the body and main container
-    var viewportHeight = window.innerHeight;
-    document.body.style.height = viewportHeight + 'px';
-    document.querySelector('.container').style.height = viewportHeight + 'px';
-}
-
-// Call the adjustHeight function when the window is resized
-window.addEventListener('resize', adjustHeight);
-
-// Initial call to adjustHeight to set the height on page load
-adjustHeight();
-
-document.addEventListener("DOMContentLoaded", function() {
-    const loadingPage = document.getElementById("loading-page");
-    const mainContent = document.getElementById("main-content");
-
-    window.onload = function() {
-        loadingPage.style.display = "none";
-        mainContent.style.display = "block";
-    };
-});
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    
-    document.getElementById("navbar").style.background = "#501e27";
-  } else {
-   
-    document.getElementById("navbar").style.background = "none";
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d2d74da37772588f204c43535ee1ae8a0cfb52d0e07a3a10eb83953099875e90
+size 1147
